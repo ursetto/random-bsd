eed19f068c3ad37803125fa9abe535d623b8ac10
Jim Ursetto <jim@3e8.org>
2011-07-07 00:37:42 -0500

    Disable fxrand; remove int cast in fxrandom allowing full fixnum range on 64-bit

--- bsd-random.scm
+++ bsd-random.scm
@@ -1,5 +1,7 @@
 (module bsd-random
-(randomize random fxrandom fxrand fprand)
+(randomize random fxrandom fprand
+           ;; fxrand
+           )
 
 (import scheme chicken foreign)
 
@@ -15,8 +17,6 @@
       (_srandom seed)
       (_srandomdev)))
 
-(define fxrand
-  (foreign-lambda int "freebsd_random"))   ;; Maybe subtract 2^30 so it fits in fixnum?
 (define fprand
   (foreign-lambda* double ()
     "return(freebsd_random() / (RAND_MAX + 1.0));"))
@@ -26,12 +26,22 @@
     "return(trunc(n * (freebsd_random() / (RAND_MAX + 1.0))));"))
 
 ;; % might be ok too
-(foreign-declare "#define fxrandom(n) C_fix((int)(C_unfix(n) * (freebsd_random() / (RAND_MAX + 1.0))))")
+(foreign-declare "#define fxrandom(n) C_fix((C_unfix(n) * (freebsd_random() / (RAND_MAX + 1.0))))")
 
 ;; behave like core random: only allow exact input; return garbage for exact input > 2^31 (??)
 (define (fxrandom n)
   (##sys#check-exact n 'fxrandom)
   (##core#inline "fxrandom" n))
+
+;; fxrand disabled.  On 64-bit system we can use entire 31-bit
+;; precision, but on 32-bit system we get undefined behavior
+;; due to SHL on signed int (in practice, will just become negative).
+;; Possible solution is to unequivocally SHR 1 first, reducing precision
+;; to 30 bits on all platforms; or use two code paths; or retain 31 bits by
+;; subtracting 2^30 so it fits in fixnum but can be negative.
+;;
+;; (define fxrand
+;;   (foreign-lambda int "freebsd_random"))
 
 (randomize)
 )
